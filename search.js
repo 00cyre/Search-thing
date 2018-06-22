36b52d861cfe68b7a9188bf049c8f84b45d76aad
adsvunu <zer0games2@hotmail.com>
2018-06-21 23:30:54 -0300

    Search
    :)

---
+++ search.js
@@ -0,0 +1,39 @@
+var keys = [];
+var str = "";
+var texto;
+	$('#currentQuestao').bind("DOMSubtreeModified",function(){
+	  data = null;
+	});
+	window.addEventListener("keydown",
+		function(e){
+		keys.push(e.key);
+		var streng = keys;
+		console.clear();
+		if(keys[keys.length-1] == 'F4')
+		{
+			texto = prompt();
+		}
+		if (texto != null) {
+			$.get("https://www.googleapis.com/customsearch/v1?key={KEY}&cx={CX}="
+			+ texto, function(data) 
+			{
+				if ($( ".areaEnunciado" ).find( "strong" )["length"] != 0)
+				{
+					if ($( ".areaEnunciado" ).find( "strong" )[0]["outerText"].length > 20)
+					{
+						$(".areaEnunciado").html($(".areaEnunciado").html() + "<p><strong>"+data["items"][0]["snippet"] + "</strong></p>")
+					}
+					else
+					{
+						$(".areaEnunciado").html($(".areaEnunciado").html() + "<p>"+data["items"][0]["snippet"] + "</p>")
+					}
+				}
+				else
+				{
+					$(".areaEnunciado").html($(".areaEnunciado").html() + "<p>"+data["items"][0]["snippet"] + "</p>")
+				}
+			});
+			texto = null;
+		}
+		},false);
+		console.clear();
